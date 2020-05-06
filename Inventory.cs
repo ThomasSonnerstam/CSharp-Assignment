d82d48b84f25e43d4d3dbd49affcfcd4a5d39343
Thomas Sönnerstam <sonnerstam.thomas@hotmail.com>
2020-05-06 21:44:33 +0200

    Inventory done, bank in progress

--- Inventory.cs
+++ Inventory.cs
@@ -4,12 +4,24 @@ namespace CSharpAssignment
 {
     public class Inventory
     {
-        public List<string> GoodsAvailable = new List<string> { "eggs", "milk", "apples", "bananas", "juice", "chicken" };
+        public Dictionary<string, int> GoodsAvailable = new Dictionary<string, int>();
         public List<string> MyGoods = new List<string>();
+
+        public Inventory()
+        {
+            GoodsAvailable.Add("eggs", 30);
+            GoodsAvailable.Add("juice", 25);
+            GoodsAvailable.Add("chicken", 60);
+            GoodsAvailable.Add("bread", 20);
+            GoodsAvailable.Add("cucumber", 15);
+            GoodsAvailable.Add("beef", 80);
+            GoodsAvailable.Add("salad", 25);
+        }
+        
 
         public void Buy(string item)
         {
-            if (GoodsAvailable.Contains(item))
+            if (GoodsAvailable.ContainsKey(item))
             {
                 MyGoods.Add(item);
             }
--- Program.cs
+++ Program.cs
@@ -7,32 +7,21 @@ namespace CSharpAssignment
     class Program
     {
 
-        enum Commands
-        {
-            bank,
-            inventory,
-            history,
-            vendingMachine,
-            yes,
-            no,
-            withdraw,
-            numberOfCommands
-        }
-        
         static void Main(string[] args)
         {
             Inventory inventory = new Inventory();
             Bank bank = new Bank();
             
             Console.WriteLine("Hello and welcome to the best virtual experience ever.");
-            Console.WriteLine("1. Access your bank");
-            Console.WriteLine("2. Vending machine");
-            Console.WriteLine("0. Exit your virtual experience.");
-            
+
             string commandInput = "";
 
             while (!commandInput.Equals("0"))
             {
+                Console.WriteLine("1. Access your bank");
+                Console.WriteLine("2. Vending machine");
+                Console.WriteLine("0. Exit your virtual experience.");
+                
                 commandInput = Console.ReadLine();
                 
                 if (commandInput == "1")
@@ -47,6 +36,8 @@ namespace CSharpAssignment
                         Console.WriteLine("2. Withdraw");
                         Console.WriteLine("3. Deposit");
                         Console.WriteLine("0. Exit from bank");
+                        
+                        bankInput = Console.ReadLine();
                         
                         if (bankInput == "1")
                         {
@@ -74,7 +65,23 @@ namespace CSharpAssignment
                         }
                         else if (bankInput == "3")
                         {
-                            
+                            Console.WriteLine("How much would you like to deposit?");
+                            int depositInput;
+                            if (Int32.TryParse(Console.ReadLine(), out depositInput))
+                            {
+                                if (bank.Deposit(depositInput))
+                                {
+                                    Console.WriteLine("Deposit successful");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Something went wrong. Try again.");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("You failed to enter a valid number.");
+                            }
                         }
                     }
                 }
@@ -84,11 +91,9 @@ namespace CSharpAssignment
                     Console.WriteLine("Welcome to the vending machine! Type \"goods\" to see what's in stock or \"history\" to see your purchase history.");
                     break;
                 }
-
-                Console.WriteLine("Wrong in command, try again.");
-            
             }
-
+            
+            Console.WriteLine("Bye bye");
         }
     }
 }
